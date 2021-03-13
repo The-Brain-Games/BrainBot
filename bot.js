d0d049e1bb1e5384cbbb50acd7595fe563b53ec2
Grant <grant@rbgn.ca>
2021-03-13 11:25:46 -0500

    added `.eval` command
    slash commands still dont work

--- bot.js
+++ bot.js
@@ -27,4 +27,29 @@ client.on("interactionCreate", (interaction) => {
     }
 });
 
+client.on('message', message => {
+    if (message.content.startsWith(".eval")) { // .eval command
+        if(message.author.id != "411883159408476160") {
+            message.reply("you do not have permission to use this command.")
+            return;
+        } else {
+        var result = message.content.split(" ").slice(1).join(" ")
+            let evaled = eval(result);
+            console.log("Running eval command:")
+            console.log(`Input: ${result}`)
+            console.log(`Output: ${evaled}`)
+
+            // message.channel.send(`result:\n${evaled}`) // <-- to send without an embed
+            const evalEmbed = new Discord.MessageEmbed()
+                .setColor('#ba365b')
+                .setTitle('Evaluation')
+                .setDescription(`Input:\n\`\`\`js\n${result}\n\`\`\`\nOutput:\n\`\`\`\n${evaled}\n\`\`\``)
+                .setTimestamp()
+                .setFooter('Evaluated by BrainBot', 'https://i.imgur.com/AkAd7Qo.png');
+
+            message.channel.send(evalEmbed);
+        }
+    }
+});
+
 client.login(TOKEN);
