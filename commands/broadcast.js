4cf47bdba34cbf173726f9e999322ea5e12bf25d
Grant <grant@rbgn.ca>
2021-04-26 18:07:25 -0400

    final touches

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, message, args) => {
     if(message.author.id != "411883159408476160" && message.author.id != "171083353502646272") {
         return message.reply("you do not have permission to use this command.");
     } else {
-        console.warn(`${message.author.tag} is broadcasting.`)
+        console.warn(`${message.author.tag} is broadcasting.`);
     }
 
     //For testing:
     //var serverIDs = ["796788484714528818", "796788504569839666"];
 
-    var serverIDs = ["804470933514879026", "789660320540393482"]; // this is the list of server channel ID's for sending to
+    var serverIDs = ["804470933514879026", "789660320540393482", "763167820014944336"]; // this is the list of server channel ID's for sending to
     var serverChannels = [];
     try {
         for (var i = 0; i < serverIDs.length; i++) {
@@ -26,6 +26,8 @@ module.exports.run = async (client, message, args) => {
     for (var i = 0; i < serverChannels.length; i++) {
         serverChannels[i].send(broadcast_message);
     }
+
+    return message.reply("I have pushed your broadcast. :)");
 }
 
 //The command's name
