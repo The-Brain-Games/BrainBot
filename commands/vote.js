c0ac3c51c19203b372bb46658916b182d8cd10ba
Grant <grant@rbgn.ca>
2021-06-07 23:38:24 -0400

    add vote command

---
+++ commands/vote.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, disbut, message, args) => {
+    return;
+}
+
+//The command's name
+module.exports.help = {
+  name: "stats",
+  description: "Shows the stats of Brain Games."
+}
