24a4756eb9a04a7065c94607668d050c34e897ff
Grant <grant@rbgn.ca>
2021-06-11 22:13:32 -0400

    update to work with one server

--- commands/stats.js
+++ commands/stats.js
@@ -1,12 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, disbut, message, args) => {
-    const brainNG = client.guilds.get("804162605107249213");
-    const brainCraft = client.guilds.get("789657841895735337");
-
-    const memberCount = brainNG.memberCount + brainCraft.memberCount;
-
-    return message.channel.send(`Brain Games is currently serving ${memberCount} members.`)
+  return message.channel.send(`Brain Games is currently serving ${client.guilds.get("804162605107249213").memberCount} members.`);
 }
 
 //The command's name
