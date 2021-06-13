79447cf8654e22e27da1b6fbfd7d7f420d04ebc9
Grant <grant@rbgn.ca>
2021-06-13 19:15:13 -0400

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, disbut, message, args) => {
-  return message.channel.send(`Brain Games is currently serving ${client.guilds.get("789657841895735337").members.size} members.`);
+  let guild = client.guilds.get("789657841895735337");
+  let memberCount = guild.members.size;
+
+  return message.channel.send(`Brain Games is currently serving ${memberCount} members.`);
 }
 
 //The command's name
